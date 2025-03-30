57aff9456cf5711c6a1d38307aad95b3c9f5cc32
caio.imperial <scaioimperial@gmail.com>
2025-03-30 16:07:51 -0300

    feat(storage): refactor method in handler

--- magiclens/storage/storage_handler.py
+++ magiclens/storage/storage_handler.py
@@ -2,5 +2,10 @@ class StorageHandler:
     def __init__(self, path: str):
         self.path = path
 
-    def save_image(self, image: str):
+    def save(self, filename: str, image: str) -> str:
         pass
+
+    def get(self, filename: str) -> str:
+        pass
+
+
