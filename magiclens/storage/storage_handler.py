6e73c46f96187f1304f7d87bdd00b01e177e089c
caio.imperial <scaioimperial@gmail.com>
2025-03-30 16:10:02 -0300

    feat(storage): image renomeado para content

--- magiclens/storage/storage_handler.py
+++ magiclens/storage/storage_handler.py
@@ -2,7 +2,7 @@ class StorageHandler:
     def __init__(self, path: str):
         self.path = path
 
-    def save(self, filename: str, image: str) -> str:
+    def save(self, filename: str, content: any) -> str:
         pass
 
     def get(self, filename: str) -> str:
