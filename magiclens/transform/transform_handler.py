645199691b8a066c7f430e949658d123016a0c92
caio.imperial <scaioimperial@gmail.com>
2025-04-12 16:04:14 -0300

    feat(transformation): mplementado a transformacao de imagens

--- magiclens/transform/transform_handler.py
+++ magiclens/transform/transform_handler.py
@@ -11,15 +11,18 @@ class TransformHandler:
         image = Image.open(filename)
         original_width, original_height = image.size
         
-        if width is None:
+        if width is None and height is None:
             width = original_width
-        if height is None:
             height = original_height
+        elif width is None:
+            width = int((height / original_height) * original_width)
+        elif height is None:
+            height = int((width / original_width) * original_height)
         
-        image.resize((width, height))
-        
+        image_resized = image.resize((width, height))
+
         image_byte_arr = BytesIO()
-        image.save(image_byte_arr, format="JPEG")
+        image_resized.save(image_byte_arr, format="JPEG")
         image_byte_arr.seek(0)
 
         return image_byte_arr.getvalue()
