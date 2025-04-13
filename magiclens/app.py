a3b8f4e00001eeffa0e3bcb716449ad166e6764c
caio.imperial <scaioimperial@gmail.com>
2025-04-13 01:04:14 -0300

    refactor(app): reoganizado as rotas da api para um arquivo api.py na pasta routes

--- magiclens/app.py
+++ magiclens/app.py
@@ -1,103 +1,12 @@
 import os
 
-from flask import Flask, request, jsonify, redirect, url_for
+from flask import Flask, redirect
 
 from magiclens.routes.view import view_image_bp
-from magiclens.storage.local_handler import LocalHandler
-from magiclens.transform.transform_handler import TransformHandler
+from magiclens.routes.api import api_image_bp
+
 
 app = Flask(__name__)
 
 app.register_blueprint(view_image_bp, url_prefix='/view')
-
-@app.route('/api/status')
-def index():
-    return {'message': 'Ok'}
-
-@app.route('/api/image', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return jsonify({"error": "Nenhum arquivo enviado"}), 400
-
-    file = request.files['file']
-    filename = file.filename
-
-    if file.filename == '':
-            return jsonify({"error": "Nome do arquivo vazio"}), 400
-    
-    filename = request.form.get('filename')
-    if filename:
-        filename = '.'.join([
-            filename.split('.')[0],
-            file.filename.split('.')[-1]
-        ])
-    else:
-        filename = file.filename
-
-    storage = LocalHandler()
-    
-    image_path = storage.save(
-        filename=filename,
-        content=file.read()
-    )
-
-    return redirect(
-        location=url_for(
-            endpoint='static', 
-            filename=image_path
-        ),
-        code=302
-    )
-
-
-@app.route('/api/image/<path:filename>', methods=['GET'])
-def process(filename: str):
-    height = request.args.get('h', type=int)
-    width = request.args.get('w', type=int)
-    flip = request.args.get('flip')
-    
-    path_list = [str(part) for part in [f'w{width}', f'h{height}', filename] if part not in [None, 'hNone', 'wNone']]
-
-    storage = LocalHandler()
-    
-    image_default_path= storage.get_path(filename)
-
-    if image_default_path is None:
-        return jsonify({"error": "Imagem original não encontrada"}), 404
-
-    full_path = os.path.join(*path_list)
-    
-    image_modified_path = storage.get_path(full_path)
-
-    # deve verificar se o imagem existe com as caracteristicas do arquivo
-    if  image_modified_path is None:
-        transform = TransformHandler()
-        
-        image_bytes = storage.get(filename)
-        image_modified_bytes = transform.change_size(
-            image_content=image_bytes,
-            width=width,
-            height=height,
-        )
-        image_modified_path = storage.save(
-            filename=full_path,
-            content=image_modified_bytes
-        )
-
-        return redirect(
-            location=url_for(
-                endpoint='static', 
-                filename=image_modified_path
-            ),
-            code=302
-        )
-    
-    return redirect(
-        location=url_for(
-            endpoint='static', 
-            filename=image_modified_path
-        ),
-        code=302
-    )
-
-
+app.register_blueprint(api_image_bp, url_prefix='/api')
